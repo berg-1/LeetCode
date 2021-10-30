07b2c564840f3781a803caf36e6bc51fc0df8d35
berg-1 <884441972@qq.com>
2021-10-30 17:29:01 +0800

    modify pom.xml
    remove annotation dependency

--- Algorithm/src/main/java/data_structure/day7/max_area_of_island/Solution.java
+++ Algorithm/src/main/java/data_structure/day7/max_area_of_island/Solution.java
@@ -1,9 +1,36 @@
 package data_structure.day7.max_area_of_island;
 
+/**
+ * 岛屿最大面积
+ * 递归
+ * 广度优先搜索
+ * 深度优先搜索
+ */
+
 public class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int area = 0;
-        return area;
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    max = Math.max(dfs(grid, i, j), max);
+                }
+            }
+        }
+        return max;
+    }
+
+    public int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        int count = 1;
+        count += dfs(grid, i + 1, j);
+        count += dfs(grid, i - 1, j);
+        count += dfs(grid, i, j + 1);
+        count += dfs(grid, i, j - 1);
+        return count;
     }
 }
 
